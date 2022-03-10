1ab4d718eae31ba86955aa98c2c62fb9ac0f3dda
Henrique <pedrohsvalente@gmail.com>
2022-03-10 14:38:26 +0000

    Added the second algorithm

--- Proj 1/Matrix.java
+++ Proj 1/Matrix.java
@@ -57,6 +57,36 @@ public class Matrix {
         System.out.println("Time: " + elapsedTimeInSecond + " seconds");
     }
 
+    static void onMultLine(){
+
+        double[][] pha = new double[line][column]; 
+        double[][] phb = new double[line][column]; 
+        double[][] phc = new double[line][column]; 
+
+        for(int i=0; i<line; i++)
+		    for(int j=0; j<line; j++)
+			    pha[i][j] = (double)1.0;
+
+	    for(int i=0; i<column; i++)
+		    for(int j=0; j<column; j++)
+			    phb[i][j] = (double)(i+1);
+
+        long startTime = System.nanoTime(); //Onde começa a contar o tempo
+
+        for(int i=0; i<line; i++)
+            for(int k=0; k<line; k++)
+                for(int j=0; j<column; j++)
+                    phc[i][j] += pha[i][k] * phb[k][j];
+
+        long stopTime = System.nanoTime(); //Onde acaba de contar
+        long elapsedTime = stopTime - startTime; //Cálculo do tempo que passou
+        double elapsedTimeInSecond = (double) elapsedTime / 1_000_000_000;//Converter em segundos
+
+
+        printFirstLine(phc, column); //Imprime 10 elementos da matriz
+        System.out.println("Time: " + elapsedTimeInSecond + " seconds");
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("1. Multiplication");
@@ -72,6 +102,10 @@ public class Matrix {
         switch (selected) {
             case 1:
                 onMult();
+                break;
+
+            case 2:
+                onMultLine();
                 break;
         
             default:
